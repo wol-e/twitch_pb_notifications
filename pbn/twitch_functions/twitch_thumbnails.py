8a6bb9f2b21e8601dfd468b5644a0b7737868ebf
Wolfgang <wolfgang.spindeler@gmail.com>
2022-05-15 22:18:31 +0200

    refactor project structure

---
+++ pbn/twitch_functions/twitch_thumbnails.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import requests
+from io import BytesIO
+from datetime import datetime
+from pathlib import Path
+
+def save_thumbnail(user_logins, twitch_client, dir, width=1600, height=900):
+    Path(dir).mkdir(parents=True, exist_ok=True)
+    streams = twitch_client.get_streams(user_logins=user_logins)[:len(user_logins)]
+    filenames = []
+    for stream in streams:
+        response = requests.get(stream["thumbnail_url"].format(width=width, height=height))
+        img = Image.open(BytesIO(response.content))
+        filename=f"{dir}/{stream['user_login']}_{str(datetime.now().timestamp()).split('.')[0]}.png"
+        img.save(filename)
+        filenames.append(filename)
+
+    return filenames
