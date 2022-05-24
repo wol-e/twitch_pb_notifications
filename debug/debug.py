6a2f9f29ea7f2471d95adee758895969f9d7aeef
Wolfgang <wolfgang.spindeler@gmail.com>
2022-05-24 21:36:23 +0200

    da bot

--- debug/debug.py
+++ debug/debug.py
@@ -3,4 +3,4 @@ from context import pbn
 from pbn import process_stream
 from pbn.twitch_functions import get_client
 
-process_stream.check_for_online(["armadaugs"])
+process_stream.check_for_online(["z4t0x"])
---
+++ pbn/telegram_bot/wolle_bot_bot.py
@@ -0,0 +1,27 @@
+import logging
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(os.getenv('wolle_bot_bot_token')).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
+
